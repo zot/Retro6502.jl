2cda3e2483efe871376efcf3e2dff5740aab12d2
Bill Burdick <bill.burdick@gmail.com>
2024-01-15 11:53:54 +0200

    buffered painting & event compression

--- Fake6502.jl/src/c64.jl
+++ Fake6502.jl/src/c64.jl
@@ -1,4 +1,5 @@
 module C64
+using Base: check_open
 using ..Fake6502: Machine, NewMachine, A, display_chars, diag, CONDENSE_START, loadprg, screen, run, step
 using ..Fake6502: ROM, init_rom, Addr, AddrRange, intRange, hex
 using ..Fake6502: register, print_n, call_6502, call_frth, reset
@@ -74,6 +75,7 @@ screen2ascii(char) = SCREEN_CODES(UInt8(char))
 
 @kwdef mutable struct C64_machine
     renderer::Ptr{SDL_Renderer}
+    screen::Ptr{SDL_Texture}
     all_dirty::Bool = true
     dirty_characters::Array{Bool, 1} = zeros(Bool, (40*25,)) # characters that have changed
     dirty_character_defs::Array{Bool, 1} = zeros(Bool, (256,)) # character defs that have changed
@@ -100,13 +102,13 @@ function with_sdl(func::Function)
     SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 16)
     
     @assert SDL_Init(SDL_INIT_EVERYTHING) == 0 "error initializing SDL: $(unsafe_string(SDL_GetError()))"
-    win = SDL_CreateWindow("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_SHOWN)
+    win = SDL_CreateWindow("C64", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, SDL_WINDOW_SHOWN)
     SDL_SetWindowResizable(win, SDL_TRUE)
     renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC)
-    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT)
+    #SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT)
     SDL_RenderClear(renderer)
     try
-        func(renderer)
+        func(renderer, win)
     finally
         SDL_DestroyWindow(win)
         SDL_Quit()
@@ -189,10 +191,23 @@ end
 
 c64(mach::Machine)::C64_machine = mach.properties[:c64]
 
-function update_screen(mach::Machine)
+function update_screen(mach::Machine, win)
     c = c64(mach)
     all_dirty = c.all_dirty
-    renderer = c.renderer
+    rend = c.renderer
+    fmt = Ref(UInt32(0))
+    check(SDL_QueryTexture)(c.screen, Ptr{UInt32}(pointer_from_objref(fmt)), C_NULL, C_NULL, C_NULL)
+    info = Ref(SDL_RendererInfo(C_NULL, UInt32(0), UInt32(0), (zeros(UInt32, 16)...,), UInt32(0), UInt32(0)))
+    check(SDL_GetRendererInfo)(rend, pointer_from_objref(info))
+    found = false
+    for i in 1:info[].num_texture_formats
+        if fmt[] == info[].texture_formats[i]
+            found = true
+        end
+    end
+    !found &&
+        error("Unsupported texture format, cannot draw screen on window")
+    check(SDL_SetRenderTarget)(rend, c.screen)
     dirtydefs = Set(i for (i,d) in enumerate(c.dirty_character_defs) if d)
     isempty(dirtydefs) && !any(c.dirty_characters) &&
         return false
@@ -200,7 +215,7 @@ function update_screen(mach::Machine)
     char_mem = character_mem(mach)
     for col in 0:39
         for pixel in 0 : 7
-            x = col * 7 + pixel
+            x = col * 8 + pixel
             for row in 0:24
                 i = row * 40 + col
                 char = scr_mem[1 + i]
@@ -214,8 +229,8 @@ function update_screen(mach::Machine)
                     y = row * 8 + rowbyte
                     pixels = char_mem[1 + 8 * char + rowbyte]
                     color = (pixels >> (7 - pixel)) & 1 == 1 ? fg : bg
-                    SDL_SetRenderDrawColor(renderer, color...)
-                    SDL_RenderDrawPoint(renderer, x, y)
+                    check(SDL_SetRenderDrawColor)(rend, color...)
+                    check(SDL_RenderDrawPoint)(rend, x, y)
                 end
             end
         end
@@ -224,29 +239,73 @@ function update_screen(mach::Machine)
     c.dirty_character_defs .= false
     c.all_dirty = false
     #error("burp")
-    SDL_RenderPresent(renderer)
+    SDL_RenderPresent(rend)
+    draw_screen(mach, win)
     #SDL_Delay(10)
     return true
 end
 
 struct Close <: Exception end
-prev_evt = 0
 
-function check_close(renderer)
-    event_ref = Ref{SDL_Event}()
-    Bool(SDL_PollEvent(event_ref))
+prev_evt = nothing
+last_draw = 0
+
+function draw_screen(mach::Machine, win)
+    c = c64(mach)
+    SDL_SetRenderTarget(c.renderer, C_NULL)
+    w = Ref{Int}(0)
+    h = Ref{Int}(0)
+    SDL_RenderGetLogicalSize(c.renderer, pointer_from_objref(w), pointer_from_objref(h))
+    if w[] == 0 && h[] == 0
+        SDL_GetWindowSize(win, pointer_from_objref(w), pointer_from_objref(h))
+    end
+    maxw = Int(round(Float64(h[]) / SCREEN_HEIGHT * SCREEN_WIDTH))
+    if w[] > maxw + 1
+        w[] = maxw
+    else
+        h[] = Int(round(Float64(w[]) / SCREEN_WIDTH * SCREEN_HEIGHT))
+    end
+    rect = Ref(SDL_Rect(0, 0, w[], h[]))
+    check(SDL_SetRenderDrawColor)(c.renderer, 0, 0, 0, 255)
+    # this doesn't work with check() for some reason
+    SDL_RenderClear(c.renderer)
+    check(SDL_RenderCopy)(c.renderer, c.screen, C_NULL, pointer_from_objref(rect))
+    SDL_RenderPresent(c.renderer)
+end
+
+as(T::Type, obj::SDL_Event) = as(T, Ref(obj))
+as(T::Type, obj::Ref{SDL_Event}) = unsafe_load(Ptr{T}(pointer_from_objref(obj)))
+
+function is_draw_evt(evt)
+    (isnothing(evt) || evt.type != SDL_WINDOWEVENT) &&
+        return false
+    evt = as(SDL_WindowEvent, evt)
+    return evt.event ∈ (SDL_WINDOWEVENT_EXPOSED, SDL_WINDOWEVENT_MAXIMIZED, SDL_WINDOWEVENT_SHOWN, SDL_WINDOWEVENT_RESTORED, SDL_WINDOWEVENT_SIZE_CHANGED, SDL_WINDOWEVENT_RESIZED)
+end
+
+function check_close(renderer, win, mach::Machine)
+    global prev_evt
+    global last_draw
+
+    event_ref = Ref{SDL_Event}(SDL_Event((zeros(UInt8, 56)...,)))
+    SDL_PollEvent(event_ref)
+    common = as(SDL_CommonEvent, event_ref)
     evt = event_ref[]
-    evt_ty = evt.type
-    if evt.type != SDL_POLLSENTINEL && prev_evt != evt.type
-        println(evt.type)
-        global prev_evt = evt.type
+    prev_type = !isnothing(prev_evt) ? as(SDL_CommonEvent, prev_evt).type : nothing
+    if is_draw_evt(prev_evt) && (prev_type != common.type || common.timestamp - last_draw > 100)
+        draw_screen(mach, win)
+        prev_evt = nothing
+        last_draw = common.timestamp
+    elseif isnothing(prev_evt) || prev_type != common.type
+        prev_evt = event_ref[]
     end
-    if evt.type == SDL_WINDOWEVENT
-        if evt.window.event ∈ (SDL_WINDOWEVENT_EXPOSED, SDL_WINDOWEVENT_MAXIMIZED, SDL_WINDOWEVENT_SHOWN, SDL_WINDOWEVENT_RESTORED, SDL_WINDOWEVENT_SIZE_CHANGED, SDL_WINDOWEVENT_RESIZED)
-            SDL_RenderPresent(renderer)
-        end
-    end
-    return evt_ty == SDL_QUIT
+    return common.type == SDL_QUIT
+end
+
+check(func::Function) = (args...)-> begin
+    result = func(args...)
+    result != 0 && error("Error $result calling $func: $(unsafe_string(SDL_GetError()))")
+    return 0
 end
 
 function init_c64(mach::Machine)
@@ -270,10 +329,10 @@ function init_c64(mach::Machine)
 end
 
 function test_c64()
-    #global mach = NewMachine()
     global mach = NewMachine(; write_func = c64_write_mem)
-    with_sdl() do renderer
-        state = C64_machine(; renderer)
+    with_sdl() do renderer, win
+        screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT)
+        state = C64_machine(; renderer, screen)
         mach.properties[:c64] = state
         init_c64(mach)
         println("ROM MEM: ", hex(ROM[BASIC_ROM.first.value]))
@@ -283,7 +342,7 @@ function test_c64()
         addrs = Dict(addr => name for (name, addr) in labels)
         maxwid = max(length.(string.(keys(labels)))...)
         state.all_dirty = true
-        update_screen(mach)
+        update_screen(mach, win)
         try
             nextupdate = UPDATE_PERIOD
             mach.step = function(mach::Machine)
@@ -301,10 +360,10 @@ function test_c64()
                 end
                 step(mach)
                 if mach.emu.clockticks >= nextupdate
-                    update_screen(mach)
+                    update_screen(mach, win)
                     nextupdate += UPDATE_PERIOD
                 end
-                if check_close(renderer)
+                if check_close(renderer, win, mach)
                     mach.cpu.s = 0
                 end
             end
@@ -325,7 +384,7 @@ function test_c64()
 
             run(mach, labels[:main]; max_ticks = 10000)
             state.all_dirty = true
-            update_screen(mach)
+            update_screen(mach, win)
         catch err
             if !(err isa Close)
                 @error "error running instructions" exception=(err,catch_backtrace())
@@ -334,7 +393,7 @@ function test_c64()
         finally
             diag(mach)
         end
-        while !check_close(renderer)
+        while !check_close(renderer, win, mach)
         end
     end
     display_chars(screen_mem(mach)) do c; SCREEN_CODES[c + 1]; end
